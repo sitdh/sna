bd606b16287e1c7a11a35a70864922f0eaed9474
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-09-07 08:44:36 +0700

    create data model file

---
+++ sna/application_datamodel.py
@@ -0,0 +1,19 @@
+class ApplicationDatamodel:
+    def __init__(self):
+        pass
+
+    @property
+    def app_id(self):
+        pass
+
+    @property
+    def app_secret(self):
+        pass
+
+    @property
+    def oauth_token(self):
+        pass
+
+    @property
+    def oauth_secret(self):
+        pass
