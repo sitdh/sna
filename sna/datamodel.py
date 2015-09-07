469d9f1570f136c50f99e26cda40d7fb9e522399
Sitdhibong Laokok <sitdhibong@gmail.com>
2015-09-07 08:43:12 +0700

    structured datamodel

---
+++ sna/datamodel.py
@@ -0,0 +1,19 @@
+class DataModel:
+    def __init__(self):
+        pass
+
+    @property
+    def app_id(self):
+        pass
+
+    @property
+    def app_secret(self):
+        pass
+
+    @property
+    def oauth_token(self):
+        pass
+
+    @property
+    def oauth_secret(self):
+        pass
